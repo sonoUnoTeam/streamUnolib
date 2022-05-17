153c226f3b99fedf323aa36d85cb168032733834
Pierre Chanial <pierre.chanial@apc.in2p3.fr>
2022-05-17 11:40:42 +0200

    Use content_type for annotations.

--- streamunolib/content_types.py
+++ streamunolib/content_types.py
@@ -1,14 +1,9 @@
 """This package contains tools to annotate transform outputs with a content type."""
-from dataclasses import dataclass
+from __future__ import annotations
 
-COMMON_MIMETYPES = {
-    'jpg': 'image/jpeg',
-    'mp3': 'audio/mpeg',
-    'mp4': 'video/mp4',
-    'png': 'image/png',
-    'svg': 'image/svg+xml',
-    'wav': 'audio/wav',
-}
+import mimetypes
+from dataclasses import dataclass
+from typing import Literal, Mapping
 
 
 @dataclass(frozen=True)
@@ -17,25 +12,39 @@ class ContentType:
 
     mimetype: str
 
+    @classmethod
+    def from_ext(cls, ext: str) -> ContentType:
+        """Content factory from file extension.
 
-def content_type(mimetype: str) -> ContentType:
-    """The ContentType factory.
+        Parameters:
+            extension: The file extension for which the MIME type is requested.
+
+        Returns:
+            The ContentType instance associated with the extension.
+
+        Examples:
+            >>> ContentType.from_ext('wav')
+            ContentType(mimetype='audio/wav')
+        """
+        ext = ext.lower()
+        mimetype, _ = mimetypes.guess_type(f'http://file.{ext}')
+        if mimetype is None:
+            raise ValueError(f'No MIME type known for extension {ext!r}')
+        return ContentType(mimetype)
+
+
+def content_type(ext: str) -> Mapping[Literal['content_type'], str]:
+    """Returns a mapping to annotate a content type.
 
     Parameters:
-        mimetype: The MIME type used in the content type.
+        extension: The file extension for which the MIME type is requested.
 
     Returns:
-        The ContentType instance associated with the mime type.
+        The mapping {'content_type': mimetype} where mimetype is inferred from
+        the extension.
 
     Examples:
         >>> content_type('wav')
-        ContentType(mimetype='audio/wav')
-        >>> content_type('text/csv')
-        ContentType(mimetype='text/csv')
+        {'content_type': 'audio/wav'}
     """
-    mimetype = mimetype.lower()
-    try:
-        mimetype = COMMON_MIMETYPES[mimetype]
-    except KeyError:
-        pass
-    return ContentType(mimetype)
+    return {'content_type': ContentType.from_ext(ext).mimetype}
--- tests/test_content_types.py
+++ tests/test_content_types.py
@@ -1,17 +1,15 @@
 import pytest
 
 from streamunolib import content_type
-from streamunolib.content_types import ContentType
 
 
 @pytest.mark.parametrize(
     'mimetype, expected',
     [
         ('jpg', 'image/jpeg'),
-        ('wav', 'audio/wav'),
-        ('audio/mpeg', 'audio/mpeg'),
+        ('wav', 'audio/x-wav'),
     ],
 )
 def test_content_types(mimetype: str, expected: str) -> None:
     actual = content_type(mimetype)
-    assert actual == ContentType(expected)
+    assert actual['content_type'] == expected
